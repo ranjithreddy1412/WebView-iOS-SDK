d96984cf6ae35645074b5f39b96a76ac0353055a
ranjith <ranjith.perkam14@gmail.com>
2024-11-19 11:41:45 -0600

    updated the webview to handle to message from JS

--- Sources/WebView-iOS-SDK/WebView.swift
+++ Sources/WebView-iOS-SDK/WebView.swift
@@ -8,17 +8,47 @@
 import SwiftUI
 import WebKit
 
+protocol WebViewMessageHandler {
+    func onMessageReceived(_ message: String)
+}
+
 public struct WebView: UIViewRepresentable {
     let url: URL
-
-    public init(url: URL) {
+    var messageHandler: WebViewMessageHandler
+    
+    init(url: URL, messageHandler: WebViewMessageHandler) {
         self.url = url
+        self.messageHandler = messageHandler
     }
-
+    
+    public class Coordinator: NSObject, WKScriptMessageHandler {
+        var parent: WebView
+        
+        init(parent: WebView) {
+            self.parent = parent
+        }
+        
+        public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+            if let messageBody = message.body as? String {
+                parent.messageHandler.onMessageReceived(messageBody)
+            }
+        }
+    }
+    
+    public func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self)
+    }
+    
     public func makeUIView(context: Context) -> WKWebView {
-        WKWebView()
+        let config = WKWebViewConfiguration()
+        let contentController = WKUserContentController()
+        contentController.add(context.coordinator, name: "iosHandler")
+        config.userContentController = contentController
+        
+        let webView = WKWebView(frame: .zero, configuration: config)
+        return webView
     }
-
+    
     public func updateUIView(_ uiView: WKWebView, context: Context) {
         let request = URLRequest(url: url)
         uiView.load(request)

d80b736d8df010a1b265942f1d7fa8067e50ead9
ranjith <ranjith.perkam14@gmail.com>
2024-11-22 08:56:56 -0600

    updated webview to support two-way communication

--- Sources/WebView-iOS-SDK/WebView.swift
+++ Sources/WebView-iOS-SDK/WebView.swift
@@ -15,10 +15,12 @@ public protocol WebViewMessageHandler {
 public struct WebView: UIViewRepresentable {
     let url: URL
     var messageHandler: WebViewMessageHandler
+    @Binding var isDismissed: WebViewIOSSDK.ViewType?
     
-    public init(url: URL, messageHandler: WebViewMessageHandler) {
+    public init(url: URL, messageHandler: WebViewMessageHandler, isDismissed: Binding<WebViewIOSSDK.ViewType?>) {
         self.url = url
         self.messageHandler = messageHandler
+        self._isDismissed = isDismissed
     }
     
     public class Coordinator: NSObject, WKScriptMessageHandler {
@@ -30,10 +32,35 @@ public struct WebView: UIViewRepresentable {
         
         public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
             if let messageBody = message.body as? String {
-                parent.messageHandler.onMessageReceived(messageBody)
+                print("Received message: \(messageBody)") // Debug log
+
+                if let data = messageBody.data(using: .utf8) {
+                    do {
+                        if let json = try JSONSerialization.jsonObject(with: data) as? [String: Any],
+                           let action = json["action"] as? String,
+                           let accountNumber = json["accountNumber"] as? String,
+                           action == "dismiss" {
+                            DispatchQueue.main.async {
+                                // Pass the received message to the parent
+                                self.parent.messageHandler.onMessageReceived("Account number: \(accountNumber)")
+                                // Trigger dismissal
+                                self.parent.isDismissed = nil
+                            }
+                        } else {
+                            print("JSON format is invalid or missing keys.")
+                        }
+                    } catch {
+                        print("Failed to parse JSON message: \(error.localizedDescription)")
+                    }
+                } else {
+                    print("Message body could not be converted to data.")
+                }
+            } else {
+                print("Message body is not a valid string.")
             }
         }
     }
+
     
     public func makeCoordinator() -> Coordinator {
         return Coordinator(parent: self)
@@ -41,12 +68,24 @@ public struct WebView: UIViewRepresentable {
     
     public func makeUIView(context: Context) -> WKWebView {
         let config = WKWebViewConfiguration()
+        
+        // Set up preferences for JavaScript
+        let preferences = WKWebpagePreferences()
+        preferences.allowsContentJavaScript = true // Enable JavaScript for this navigation
+        config.defaultWebpagePreferences = preferences
+        
+        // Set up message handler
         let contentController = WKUserContentController()
         contentController.add(context.coordinator, name: "iosHandler")
         config.userContentController = contentController
         
+        // Create and return the WKWebView
         let webView = WKWebView(frame: .zero, configuration: config)
         return webView
+    }
+    
+    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("WebView finished loading")
     }
     
     public func updateUIView(_ uiView: WKWebView, context: Context) {
@@ -54,6 +93,3 @@ public struct WebView: UIViewRepresentable {
         uiView.load(request)
     }
 }
-
-
-
